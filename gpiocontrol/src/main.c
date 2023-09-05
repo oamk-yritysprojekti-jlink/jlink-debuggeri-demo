4e5fc74ce6759dd7dc830e6312d9ffbb71edbf2a
jerpism <jere.m@live.fi>
2023-09-05 10:36:50 +0300

    Yksinkertainen ohjelma, joka lukee gpio 23 (nappi 1) tilan ja sytyttää led1:n kun sitä painetaan

--- gpiocontrol/src/main.c
+++ gpiocontrol/src/main.c
@@ -11,6 +11,10 @@
 extern void configure_gpio_out(int);
 extern void write_gpio_lo(int);
 extern void write_gpio_hi(int);
+extern int read_gpio(int);
+extern void configure_gpio_in(int);
+
+extern void _start();
 
 static int leds[N_LEDS] = {28,29,30,31};
 static bool led_state[N_LEDS] = {0,0,0,0};
@@ -28,18 +32,39 @@ void toggleLed(int n){
 }
 
 int main(void){
-    printk("Configuring gpios asm\n");
+    _start();
+   /* printk("Configuring gpios asm\n");
     for (int i = 28; i <= 31; ++i){
         configure_gpio_out(i);
     }
 
-    for(;;){
+    for(int i = 0; i < 4; ++i){
+        write_gpio_hi(leds[i]);
+    }
+
+    configure_gpio_in(8);
+    configure_gpio_in(9);
+    configure_gpio_in(23);
+    configure_gpio_in(24);*/
+
+/*    for(;;){
 
         for(int i = 0; i < 4; ++i){
             toggleLed(i);
-            k_msleep(200);
+            k_msleep(100);
         }
-    }
+        if(!read_gpio(23)){
+            write_gpio_lo(leds[0]);
+        }else{
+            write_gpio_hi(leds[0]);
+        }
+        if(!read_gpio(24)){
+            write_gpio_lo(leds[1]);
+        }else{
+            write_gpio_hi(leds[1]);
+        }
+        k_msleep(200);
+    }*/
 
     return 0;
 }
